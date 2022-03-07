3888ed569b9cb7bec85b6a5b5f45d15da53fc3f2
Andrew Njoo <adnjoo@gmail.com>
2022-03-06 23:56:53 -0800

    heroku

--- controller.js
+++ controller.js
@@ -1,14 +1,23 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 const router = require("express").Router();
 require("dotenv").config();
-const { host, user, database, password, port } = require("./config");
+// local use
+// const { host, user, database, password, port } = require("./config");
 
+// const pool = new Pool({
+//   host,
+//   user,
+//   database,
+//   password,
+//   port,
+// });
+
+// deployment
 const pool = new Pool({
-  host,
-  user,
-  database,
-  password,
-  port,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const getKoans = (req, res) => {
--- db.js
+++ db.js
@@ -2,12 +2,20 @@ const { Pool } = require("pg");
 const { host, user, database, password, port } = require("./config");
 
 // Create a pool instance and pass in our config, which we set in our env vars
+// const pool = new Pool({
+//   host,
+//   user,
+//   database,
+//   password,
+//   port,
+// });
+
+// deployment
 const pool = new Pool({
-  host,
-  user,
-  database,
-  password,
-  port,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
